4d29038a864fdf710d576b9311dfd17e33fe819c
Max Roncace <me@caseif.net>
2018-10-31 18:09:23 -0400

    Make it a little more robust

--- src/main/java/net/caseif/badchunkprinter/mixin/MixinAnvilChunkLoader.java
+++ src/main/java/net/caseif/badchunkprinter/mixin/MixinAnvilChunkLoader.java
@@ -1,31 +1,30 @@
 package net.caseif.badchunkprinter.mixin;
 
-import net.caseif.badchunkprinter.Main;
-
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.NibbleArray;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.AnvilChunkLoader;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilChunkLoader.class)
 public abstract class MixinAnvilChunkLoader {
 
-    private static final String SELF_READCHUNKFROMNBT
-            = "readChunkFromNBT(Lnet/minecraft/world/World;Lnet/minecraft/nbt/NBTTagCompound;)Lnet/minecraft/world/chunk/Chunk;";
+    private static final String SELF_CHECKEDREADCHUNKFROMNBT__ASYNC
+            = "checkedReadChunkFromNBT__Async(Lnet/minecraft/world/World;IILnet/minecraft/nbt/NBTTagCompound;)[Ljava/lang/Object;";
 
-    private static final String NIBBLE_ARRAY = "net/minecraft/world/chunk/NibbleArray";
+    private static final String ANVILCHUNKLOADER_READCHUNKFROMNBT
+            = "net/minecraft/world/chunk/storage/AnvilChunkLoader.readChunkFromNBT(Lnet/minecraft/world/World;Lnet/minecraft/nbt/NBTTagCompound;)Lnet/minecraft/world/chunk/Chunk;";
 
-    @Redirect(method = SELF_READCHUNKFROMNBT, at = @At(value = "NEW", target = NIBBLE_ARRAY))
-    public NibbleArray constructNibbleArray(byte[] bytes, World world, NBTTagCompound nbt) {
+    @Shadow abstract Chunk readChunkFromNBT(World world, NBTTagCompound nbt);
+
+    @Redirect(method = SELF_CHECKEDREADCHUNKFROMNBT__ASYNC, at = @At(value = "INVOKE", target = ANVILCHUNKLOADER_READCHUNKFROMNBT))
+    public Chunk readChunkFromNBT_redirect(AnvilChunkLoader self, World world, NBTTagCompound nbt, World parentWorld, int chunkX, int chunkZ, NBTTagCompound parentNbt) {
         try {
-            return new NibbleArray(bytes);
+            return readChunkFromNBT(world, nbt);
         } catch (Throwable t) {
-            int chunkX = nbt.getInteger("xPos");
-            int chunkZ = nbt.getInteger("zPos");
-
             System.err.println("Encountered corrupted chunk at (" + chunkX + ", " + chunkZ + ").");
             throw t;
         }
