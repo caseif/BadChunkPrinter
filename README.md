e6d3c5f11487ee0301ecc523dff165fc9bf0ce5a
Max Roncace <me@caseif.net>
2018-12-26 10:49:03 -0500

    Handle errors arising from CompressedStreamTools in loadChunk__Async

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Tiny utility mod for Sponge which uses Mixin to print the location of a chunk should it fail to load.
 
-The mod works by injecting an error handler into the chunk-loading routine. The error will pass through
+The mod works by injecting error handlers into the chunk-loading routine. The error will pass through
 and the server will still crash, but the mod will print the location in chunk coordinates prior to the
 crash occurring.
 
--- src/main/java/net/caseif/badchunkprinter/mixin/MixinAnvilChunkLoader.java
+++ src/main/java/net/caseif/badchunkprinter/mixin/MixinAnvilChunkLoader.java
@@ -1,6 +1,8 @@
 package net.caseif.badchunkprinter.mixin;
 
+import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.AnvilChunkLoader;
@@ -8,6 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.DataInputStream;
+import java.io.IOException;
 
 @Mixin(AnvilChunkLoader.class)
 public abstract class MixinAnvilChunkLoader {
@@ -18,12 +23,28 @@ public abstract class MixinAnvilChunkLoader {
     private static final String ANVILCHUNKLOADER_READCHUNKFROMNBT
             = "net/minecraft/world/chunk/storage/AnvilChunkLoader.readChunkFromNBT(Lnet/minecraft/world/World;Lnet/minecraft/nbt/NBTTagCompound;)Lnet/minecraft/world/chunk/Chunk;";
 
+    private static final String SELF_LOADCHUNK__ASYNC
+            = "loadChunk__Async(Lnet/minecraft/world/World;II)[Ljava/lang/Object;";
+
+    private static final String COMPRESSEDSTREAMTOOLS_READ
+            = "net/minecraft/nbt/CompressedStreamTools.read(Ljava/io/DataInputStream;)Lnet/minecraft/nbt/NBTTagCompound;";
+
     @Shadow abstract Chunk readChunkFromNBT(World world, NBTTagCompound nbt);
 
     @Redirect(method = SELF_CHECKEDREADCHUNKFROMNBT__ASYNC, at = @At(value = "INVOKE", target = ANVILCHUNKLOADER_READCHUNKFROMNBT))
     public Chunk readChunkFromNBT_redirect(AnvilChunkLoader self, World world, NBTTagCompound nbt, World parentWorld, int chunkX, int chunkZ, NBTTagCompound parentNbt) {
         try {
             return readChunkFromNBT(world, nbt);
+        } catch (Throwable t) {
+            System.err.println("Encountered corrupted chunk at (" + chunkX + ", " + chunkZ + ").");
+            throw t;
+        }
+    }
+
+    @Redirect(method = SELF_LOADCHUNK__ASYNC, at = @At(value = "INVOKE", target = COMPRESSEDSTREAMTOOLS_READ))
+    public NBTTagCompound read_redirect(DataInputStream dataStream, World parentWorld, int chunkX, int chunkZ) throws IOException {
+        try {
+            return CompressedStreamTools.read(dataStream);
         } catch (Throwable t) {
             System.err.println("Encountered corrupted chunk at (" + chunkX + ", " + chunkZ + ").");
             throw t;
